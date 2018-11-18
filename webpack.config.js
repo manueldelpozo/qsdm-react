2f9461de124230247f1f155547fed5f8ed15457d
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2018-11-18 15:45:27 +0100

    config init

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
-};
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  };
